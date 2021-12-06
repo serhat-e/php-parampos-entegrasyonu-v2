69dbd4c4f19ffccd378e7148ba9ee124211f744b
Serhat Eyişekerciler <serhat@arenayazilim.com>
2021-12-06 16:16:12 +0300

    Update index.php
    Birkaç satırda gözden kaçan şeyler olmuş.

--- Config/index.php
+++ Config/index.php
@@ -10,9 +10,9 @@ define('URL',(isset($_SERVER["HTTPS"]) ? 'https' : 'http').'://'.$_SERVER['HTTP_
 define("WHICH","TEST"); // Hangi ortam çalıaşcak buradn belirliyoruz. TEST or PROD
 
 if(WHICH == "TEST"){
-    define("SERVICE_URL",'https://posws.param.com.tr/turkpos.ws/service_turkpos_prod.asmx');
-} else{
     define("SERVICE_URL","https://test-dmz.param.com.tr:4443/turkpos.ws/service_turkpos_test.asmx");
+} else{
+    define("SERVICE_URL",'https://posws.param.com.tr/turkpos.ws/service_turkpos_prod.asmx');
 }
 
 /**
@@ -30,8 +30,8 @@ define("GUID","0c13d406-873b-403b-9c09-a5766840d98c");
 /**
  * Başarılı ve başarısız sayfaları.
  */
-define("Hata_URL",URL."/parampos-v2/view/basarili.php");
-define("Basarili_URL",URL."/parampos-v2/view/hatali.php");
+define("Hata_URL",URL."/parampos-v2/view/hatali.php");
+define("Basarili_URL",URL."/parampos-v2/view/basarili.php");
 
 /**
  * Aşağıda örnek test kartı bilgileri hazırlanıyor.
